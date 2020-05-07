ac4d80adab6050c281f5afa07999b382582aa340
Junnna <gud0415@gmail.com>
2020-05-07 11:31:30 +0900

    fix : my_house_web side_menu, modal

--- personal_study/my_house/assets/js/index.js
+++ personal_study/my_house/assets/js/index.js
@@ -71,3 +71,19 @@ $( document ).ready(function() {
         return false;
     });
 });
+
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+     mouseX = e.pageX;
+     mouseY = e.pageY;
+    traX = ((4 * mouseX) / 350) + 10;
+    traY = ((4 * mouseY) / 350) + 20;
+    $(".title ").css({"background-position": traX + "%" + traY + "%"});
+    $(".title span").css({"background-position": traY + "%" + traX + "%"});
+
+  });
+});
