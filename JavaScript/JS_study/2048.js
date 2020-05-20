0fce496f4b83b453551949b0d7fee33e4a6bb6d5
Junnna <gud0415@gmail.com>
2020-05-20 09:54:48 +0900

    feat : mouseup, downm move

--- JavaScript/JS_study/2048.js
+++ JavaScript/JS_study/2048.js
@@ -47,3 +47,29 @@ function draw() { // 데이터를 받아서 화면에 표시를 해주는 것
 reset();
 random_create();
 draw();
+
+var drag_start = false // drag용 flag
+var start_location;
+var end_location;
+
+// ScreenX: 모니터 기준 좌표
+// pageX: 페이지(스크롤 포함)
+// clientX: 브라우저 화면 기준
+// offsetX: 이벤트 타겟 기준
+window.addEventListener('mousedown', function(e) {
+    console.log('mousedown', e);
+    drag_start = true; // mouse를 클릭했을 때 가능하도록 동작
+    start_location = [e.clientX, e.clientY];
+});
+
+window.addEventListener('mousemove', function(e) {
+    if(drag_start) { // flag가 true일 때 mousemove가 작동
+        console.log('mousemove', e);
+    }
+});
+
+window.addEventListener('mouseup', function(e) {
+    console.log('mouseup', e);
+    drag_start = false; // mouse를 놨을 때 정지
+    end_location = [e.clientX, e.clientY];
+});
