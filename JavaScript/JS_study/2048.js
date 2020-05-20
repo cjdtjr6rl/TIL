d8f6d706f3a0647a2cfb87408e122eb120d11cf0
Junnna <gud0415@gmail.com>
2020-05-20 10:26:01 +0900

    feat : drag location distinct

--- JavaScript/JS_study/2048.js
+++ JavaScript/JS_study/2048.js
@@ -48,7 +48,8 @@ reset();
 random_create();
 draw();
 
-var drag_start = false // drag용 flag
+var drag_start = false; // drag용 flag
+var draging = false; // 클릭했을 때 막기 위해 생성
 var start_location;
 var end_location;
 
@@ -57,19 +58,34 @@ var end_location;
 // clientX: 브라우저 화면 기준
 // offsetX: 이벤트 타겟 기준
 window.addEventListener('mousedown', function(e) {
-    console.log('mousedown', e);
     drag_start = true; // mouse를 클릭했을 때 가능하도록 동작
     start_location = [e.clientX, e.clientY];
 });
 
 window.addEventListener('mousemove', function(e) {
     if(drag_start) { // flag가 true일 때 mousemove가 작동
-        console.log('mousemove', e);
+        draging = true;
     }
 });
 
-window.addEventListener('mouseup', function(e) {
-    console.log('mouseup', e);
+window.addEventListener('mouseup', function(e) { // 마우스 클릭 시
     drag_start = false; // mouse를 놨을 때 정지
-    end_location = [e.clientX, e.clientY];
+    end_location = [e.clientX, e.clientY]; // 끝 좌표 (x축, y축)
+    if(draging) { // mouse가 움직이는 중 일때
+        var location;
+        var x_interval = end_location[0] - start_location[0]; // x축 좌표
+        var y_interval = end_location[1] - start_location[1]; // y축 좌표
+        if(x_interval < 0 && Math.abs(x_interval) / Math.abs(y_interval) > 1) { // x축이 0보다 작고 y축이 1보다 클 때
+            location = '왼쪽';
+        } else if(x_interval > 0 && Math.abs(x_interval) / Math.abs(y_interval) > 1) { // x축이 0보다 크고 y축이 1보다 클 때
+            location = '오른쪽';
+        } else if(y_interval > 0 && Math.abs(x_interval) / Math.abs(y_interval) < 1) { // x축이 0보다 크고 y축이 1보다 작을 때
+            location = '아래';
+        } else if(y_interval < 0 && Math.abs(x_interval) / Math.abs(y_interval) < 1) { // x축이 0보다 작고 y축이 1보다 작을 때
+            location = '위';
+        }
+        console.log(x_interval, y_interval, location);
+    }
+    drag_start = false;
+    draging = false;
 });
