d4e5c6385b57de09a681ddddd945c7f81e1615a0
Junnna <gud0415@gmail.com>
2020-06-25 23:57:25 +0900

    fix : constructor state and make component

--- React/test3/my-app/src/index.js
+++ React/test3/my-app/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    // 무언가를 기억하기 위해 만든 state
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+          // setState를 호출할 때 button을 클릭 시 Square가 다시 렌더링이 되어야 함을 알림
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
       );
     }
