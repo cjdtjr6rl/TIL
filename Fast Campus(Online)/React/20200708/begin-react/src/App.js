6b6dd6bd6e2304e7633b781c5728a58919e0ce88
Junnna <gud0415@gmail.com>
2020-07-08 18:02:33 +0900

    fix : study JSX

--- Fast Campus(Online)/React/20200708/begin-react/src/App.js
+++ Fast Campus(Online)/React/20200708/begin-react/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Hello from './Hello'
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24, // 기본 단위 px
+    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
+  }
   return (
-    <div>
+    <>
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
