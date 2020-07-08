43c6fca249708d35f0eed85c7557be90b44d7647
Junnna <gud0415@gmail.com>
2020-07-08 18:53:13 +0900

    fix : study props

--- Fast Campus(Online)/React/20200708/begin-react/src/App.js
+++ Fast Campus(Online)/React/20200708/begin-react/src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import Hello from './Hello'
-import './App.css';
+import Wrapper from './Wrapper';
+
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
   return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="purple" />
+    </Wrapper>
   );
 }
 
