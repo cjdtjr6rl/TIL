502f921659ee18621b45bf7ef371f94323a30890
Junnna <gud0415@gmail.com>
2020-07-09 18:10:35 +0900

    feat : study insert object input

--- Fast Campus(Online)/React/project/begin-react/src/App.js
+++ Fast Campus(Online)/React/project/begin-react/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+// 1.
 import Hello from './Hello'
 import Wrapper from './Wrapper';
+// 2.
+import Counter from './Counter';
+import InputSample from './InputSample';
+// 3.
 
 
 function App() {
   return (
+    <>
+    {/** 1. */}
     <Wrapper>
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial={true} />
       <Hello color="purple" />
     </Wrapper>
+    {/** 2. */}
+    <Counter />
+    {/** 3. */}
+    <InputSample />
+    </>
   );
 }
 
