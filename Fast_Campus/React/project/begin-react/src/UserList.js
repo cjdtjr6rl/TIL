1b19c3c48181e106ced0ab9ea620fa3dc927bf64
Junnna <gud0415@gmail.com>
2020-07-14 15:29:39 +0900

    feat : study useEffect

--- Fast_Campus/React/project/begin-react/src/UserList.js
+++ Fast_Campus/React/project/begin-react/src/UserList.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
+    // useEffect(() => {
+    //     console.log('컴포넌트가 화면에 나타남');
+    //     return () => {
+    //         console.log('컴포넌트가 화면에서 사라짐');
+    //     }
+    // }, []); // deps가 비어 있을 경우 컴포넌트가 처음 화면에 나타날 때에만 실행이 됨
+    useEffect(() => {
+        console.log('user값이 설정됨');
+        console.log(user);
+        return() => {
+            console.log('user값이 바뀌기 전');
+            console.log(user);
+        }
+    }, [user]);
     return (
         <div>
             <b style={{
