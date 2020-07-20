dab23c3056c4198879b92e8920cb596c111d5beb
Junnna <gud0415@gmail.com>
2020-07-20 16:48:53 +0900

    feat : study Context API (create, useContext)

---
+++ Fast_Campus/React/project/begin-react/src/ContextSample.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const MyContext = createContext('defaultValue');
+
+function Child() {
+    const text = useContext(MyContext);
+    return <div>안녕하세요? {text}</div>
+}
+
+function Parent({ text }) {
+    return <Child text={text} />
+}
+
+function GrandParent({ text }) {
+    return <Parent text={text} />
+}
+
+function ContextSample() {
+    const [value, setValue] = useState(true);
+    return (
+        <MyContext.Provider value={value ? 'Junnna' : 'Jun Hyoung'}>
+            <GrandParent />
+            <button onClick={() => setValue(!value)}>Click me!</button>
+        </MyContext.Provider>
+    );
+}
+
+export default ContextSample;
