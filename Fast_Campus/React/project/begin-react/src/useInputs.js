2701e9dbfce83d48d1f47c2493438dee9041c51f
Junnna <gud0415@gmail.com>
2020-07-20 15:56:05 +0900

    feat : convert useState to useReducer

--- Fast_Campus/React/project/begin-react/src/useInputs.js
+++ Fast_Campus/React/project/begin-react/src/useInputs.js
@@ -1,12 +1,29 @@
-import { useState, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                [action.name]: action.value
+            };
+        case 'RESET':
+            return Object.keys(state).reduce((acc, current) => {
+                acc[current] = '';
+                return acc;
+            }, {});
+        default:
+            throw new Error('Unhandled action')
+    }
+}
 
 function useInputs(initialForm) {
-    const [form, setForm] = useState(initialForm);
+    const [form, dispatch] = useReducer(reducer, initialForm);
     const onChange = useCallback(e => {
         const { name, value } = e.target;
-        setForm(form => ({ ...form, [name]: value }));
+        dispatch({ type: 'CHANGE', name, value })
     }, []);
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    const reset = useCallback(() => dispatch({ type: 'RESET' }), []);
 
     return [form, onChange, reset];
 };
