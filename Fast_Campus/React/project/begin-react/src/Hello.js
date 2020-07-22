014006314eed4b944dd12d3d30a10045388c3754
Junnna <gud0415@gmail.com>
2020-07-22 17:54:13 +0900

    feat : study class component

--- Fast_Campus/React/project/begin-react/src/Hello.js
+++ Fast_Campus/React/project/begin-react/src/Hello.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ color, name, isSpecial }) {
-    return (
-        <div style={{
-            color: color
-        }}>
-            {isSpecial ? 'Very Special ' : 'Not Special '}
-            안녕하세요. {name}
-        </div>
-    )
+class Hello extends Component {
+    static defaultProps = {
+        name: '이름없음',
+    };
+    
+    render() {
+        const { isSpecial, name, color } = this.props;
+        return (
+            <div style={{ color }}>
+                {isSpecial && <b>*</b>}
+                안녕하세요? {name}
+            </div>
+        );
+    }
 }
 
-Hello.defaultProps = {
-    name: '이름없음'
-};
+// function Hello({ color, name, isSpecial }) {
+//     return (
+//         <div style={{
+//             color: color
+//         }}>
+//             {isSpecial ? 'Very Special ' : 'Not Special '}
+//             안녕하세요. {name}
+//         </div>
+//     )
+// }
+
+// Hello.defaultProps = {
+//     name: '이름없음'
+// };
 
 export default Hello;
