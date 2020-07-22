84cff0182d11925f96d69c5a52a58762f18e1195
Junnna <gud0415@gmail.com>
2020-07-22 16:41:24 +0900

    feat : study Immer

--- Fast_Campus/React/project/begin-react/src/App.js
+++ Fast_Campus/React/project/begin-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { useReducer, useMemo, createContext } from 'react';
+import produce from 'immer';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -33,24 +34,32 @@ const initialState = {
 function reducer(state, action) {
   switch(action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
+      // return {
+      //   users: state.users.concat(action.user)
+      // };
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user => 
-          user.id === action.id
-            ? { ...user, active: !user.active }
-            : user
-        )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
+      // return {
+      //   users: state.users.map(user => 
+      //     user.id === action.id
+      //       ? { ...user, active: !user.active }
+      //       : user
+      //   )
+      // };
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+      // return {
+      //   users: state.users.filter(user => user.id !== action.id)
+      // };
     default:
       throw new Error('Unhandled action');
   }
