106c38777ba16b1d35640ad0cdd81536a2651fae
Junnna <gud0415@gmail.com>
2020-07-23 16:23:42 +0900

    feat : study use sentry

--- Fast_Campus/React/project/error-catch/src/index.js
+++ Fast_Campus/React/project/error-catch/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as Sentry from '@sentry/react';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: "https://6492f5b00192494f8f48b01c93fe599d@o424253.ingest.sentry.io/5355814"
+});
 
 ReactDOM.render(
   <React.StrictMode>
