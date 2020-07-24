7a1cef0ce6a9738c59e2c2985c8cd88b7ee6aa29
Junnna <gud0415@gmail.com>
2020-07-24 15:14:44 +0900

    feat : study setting color props and use mixin

--- Fast_Campus/React/project/styling-with-sass/src/App.js
+++ Fast_Campus/React/project/styling-with-sass/src/App.js
@@ -6,9 +6,31 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button size="large">BUTTON</Button>
-        <Button>BUTTON</Button>
-        <Button size="small">BUTTON</Button>
+        <Button color="blue" size="large">
+          BUTTON
+        </Button>
+        <Button color="blue">BUTTON</Button>
+        <Button color="blue" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">
+          BUTTON
+        </Button>
       </div>
     </div>
   );
--- Fast_Campus/React/project/styling-with-sass/src/component/Button.js
+++ Fast_Campus/React/project/styling-with-sass/src/component/Button.js
@@ -2,8 +2,12 @@ import React from "react";
 import classNames from "classnames";
 import "./Button.scss";
 
-function Button({ children, size }) {
-  return <button className={classNames("Button", size)}>{children}</button>;
+// size: small, medium, large
+// color: blue, pink, gray
+function Button({ children, size, color }) {
+  return (
+    <button className={classNames("Button", size, color)}>{children}</button>
+  );
 }
 
 Button.defaultProps = {
