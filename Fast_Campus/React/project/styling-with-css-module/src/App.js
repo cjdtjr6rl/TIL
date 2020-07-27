e75e9ddfbbd15ec9853ca9b2e8910a1a77a931d7
Junnna <gud0415@gmail.com>
2020-07-27 14:59:51 +0900

    feat : study CSS module

---
+++ Fast_Campus/React/project/styling-with-css-module/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import CheckBox from "./component/CheckBox";
+
+function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = (e) => {
+    setCheck(e.target.checked);
+  };
+  return (
+    <div>
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+    </div>
+  );
+}
+
+export default App;
