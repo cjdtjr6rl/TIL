611acd3d0822c0da3ad461c0abc0b502351d8920
Junnna <gud0415@gmail.com>
2020-07-29 15:47:01 +0900

    feat : study styled-component_make button

---
+++ Fast_Campus/React/project/styling-with-styled-components/src/component/Button.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  /* 공통 스타일 */
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  /* 크기 */
+  height: 2.25rem;
+  font-size: 1rem;
+
+  /* 색상 */
+  background: #228be6;
+  &:hover {
+    background: pink;
+  }
+  &:active {
+    background: purple;
+  }
+
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function Button({ children, ...rest }) {
+  return <StyledButton {...rest}>{children}</StyledButton>;
+}
+
+export default Button;
