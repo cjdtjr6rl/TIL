10677d20096aa70b057c35ded85c6586678d1725
Junnna <gud0415@gmail.com>
2020-08-03 17:14:19 +0900

    feat : run create, delete, display function

--- Fast_Campus/React/project/mashup-todolist/src/components/TodoCreate.js
+++ Fast_Campus/React/project/mashup-todolist/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -53,7 +54,7 @@ const InsertFormPositioner = styled.div`
   position: absolute;
 `;
 
-const InsertFrom = styled.div`
+const InsertForm = styled.form`
   background: #f8f9fa;
   padding: 32px 32px 72px;
   border-bottom-left-radius: 16px;
@@ -73,15 +74,39 @@ const Input = styled.input`
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
   const onToggle = () => setOpen(!open);
+  const onChange = (e) => setValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "CREATE",
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false,
+      },
+    });
+    setValue("");
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertFrom>
-            <Input placeholder="할 일을 입력 후, Enter를 누르세요." autoFocus />
-          </InsertFrom>
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              onChange={onChange}
+              value={value}
+              placeholder="할 일을 입력 후, Enter를 누르세요."
+              autoFocus
+            />
+          </InsertForm>
         </InsertFormPositioner>
       )}
       <CircleButton onClick={onToggle} open={open}>
@@ -91,4 +116,4 @@ function TodoCreate() {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
--- Fast_Campus/React/project/mashup-todolist/src/components/TodoList.js
+++ Fast_Campus/React/project/mashup-todolist/src/components/TodoList.js
@@ -11,14 +11,18 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-  const state = useTodoState();
-  console.log(state);
+  const todos = useTodoState();
+
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done />
-      <TodoItem text="리액트 마스터" done={false} />
-      <TodoItem text="취업하기" done={false} />
-      <TodoItem text="이력서 작성하기" done />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 }
