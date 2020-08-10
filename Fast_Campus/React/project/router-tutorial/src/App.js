24a80a52b0c88e674bac4d69816f520ee0ac3841
Junnna <gud0415@gmail.com>
2020-08-10 16:07:47 +0900

    feat : study Router Add-ons  (history, withRouter)

--- Fast_Campus/React/project/router-tutorial/src/App.js
+++ Fast_Campus/React/project/router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -15,13 +16,17 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/Profiles">프로필 목록</Link>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
+        <li>
+          <Link to="/history">예제</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
--- Fast_Campus/React/project/router-tutorial/src/Profile.js
+++ Fast_Campus/React/project/router-tutorial/src/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import WithRouterSample from "./WithRouterSample";
 
 const profileData = {
   Junnna: {
@@ -25,6 +26,7 @@ function Profile({ match }) {
         {username} ({profile.name})
       </h3>
       <p>{profile.description}</p>
+      <WithRouterSample />
     </div>
   );
 }
