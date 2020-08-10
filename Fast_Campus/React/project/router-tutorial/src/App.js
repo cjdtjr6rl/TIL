269a1d8c17c731d9e05e93e5673a81abb94275c5
Junnna <gud0415@gmail.com>
2020-08-10 15:17:56 +0900

    feat : study sub route

--- Fast_Campus/React/project/router-tutorial/src/App.js
+++ Fast_Campus/React/project/router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 function App() {
   return (
@@ -14,11 +14,14 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/Profiles">프로필 목록</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
-      <Route path="/profiles/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
