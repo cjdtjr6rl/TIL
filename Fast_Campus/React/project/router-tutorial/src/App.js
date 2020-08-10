6feafe30a826a79c9b79b6b24fa05e397d3271f0
Junnna <gud0415@gmail.com>
2020-08-10 14:58:53 +0900

    feat : study parameter, query

--- Fast_Campus/React/project/router-tutorial/src/App.js
+++ Fast_Campus/React/project/router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 }
