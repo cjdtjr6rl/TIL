1770692ca0612ad322e48933fdb98012c9e97c42
Junnna <gud0415@gmail.com>
2020-08-13 13:54:11 +0900

    feat : study redux make modules

--- Fast_Campus/React/project/learn-redux/src/index.js
+++ Fast_Campus/React/project/learn-redux/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
