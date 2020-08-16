e57f8a2b0a5b126fccdf79ebf5e8d75126034c8f
Junnna <gud0415@gmail.com>
2020-08-16 23:04:55 +0900

    feat : study redux devTools

--- Fast_Campus/React/project/learn-redux/src/index.js
+++ Fast_Campus/React/project/learn-redux/src/index.js
@@ -6,8 +6,10 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
+
 console.log(store.getState());
 
 ReactDOM.render(
