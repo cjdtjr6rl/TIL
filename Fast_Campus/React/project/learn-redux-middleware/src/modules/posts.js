fd197be1b22e6eb4afcba21c1c1d8bef3ca91911
Junnna <gud0415@gmail.com>
2020-08-24 15:39:26 +0900

    feat : study make reducerUtils and refactoring

--- Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
+++ Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postsAPI from "../api/posts";
+import { reducerUtils } from "../lib/asyncUtils";
 
 const GET_POSTS = "GET_POSTS";
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
@@ -47,3 +48,45 @@ export const getPost = (id) => async (dispatch) => {
     });
   }
 };
+
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
+};
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: reducerUtils.loading(),
+      };
+    case GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: reducerUtils.success(action.posts),
+      };
+    case GET_POSTS_ERROR:
+      return {
+        ...state,
+        posts: reducerUtils.error(action.error),
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: reducerUtils.loading(),
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: reducerUtils.success(action.post),
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: reducerUtils.error(action.error),
+      };
+    default:
+      return state;
+  }
+}
