9f87c6e607289fcabada260c5362d85804c089c8
Junnna <gud0415@gmail.com>
2020-08-25 14:14:15 +0900

    feat : util function createPromistThunkById, handledAsyncActionsById

--- Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
+++ Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
@@ -3,6 +3,8 @@ import {
   reducerUtils,
   createPromiseThunk,
   handleAsyncActions,
+  createPromistThunkById,
+  handleAsyncActionsById,
 } from "../lib/asyncUtils";
 
 const GET_POSTS = "GET_POSTS";
@@ -16,20 +18,7 @@ const GET_POST_ERROR = "GET_POST_ERROR";
 const CLEAR_POST = "CLEAR_POST";
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST, meta: id });
-  try {
-    const payload = await postsAPI.getPostById(id);
-    dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_ERROR,
-      payload: e,
-      error: true,
-      meta: id,
-    });
-  }
-};
+export const getPost = createPromistThunkById(GET_POST, postsAPI.getPostById);
 
 export const clearPost = () => ({ type: CLEAR_POST });
 
@@ -39,37 +28,7 @@ const initialState = {
 };
 
 const getPostsReducer = handleAsyncActions(GET_POSTS, "posts", true);
-const getPostReducer = (state, action) => {
-  const id = action.meta;
-  switch (action.type) {
-    case GET_POST:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
-        },
-      };
-    case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.success(action.payload),
-        },
-      };
-    case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.error(action.payload),
-        },
-      };
-    default:
-      return state;
-  }
-};
+const getPostReducer = handleAsyncActionsById(GET_POST, "post", true);
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
