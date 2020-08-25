2f8e1465f17686092fb76f4fab4c85e5b75df395
Junnna <gud0415@gmail.com>
2020-08-25 13:19:13 +0900

    feat : study post data keep and reset

--- Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
+++ Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
@@ -13,15 +13,18 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
+const CLEAR_POST = "CLEAR_POST";
+
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const clearPost = () => ({ type: CLEAR_POST });
 
 const initialState = {
   posts: reducerUtils.initial(),
   post: reducerUtils.initial(),
 };
 
-const getPostsReducer = handleAsyncActions(GET_POSTS, "posts");
+const getPostsReducer = handleAsyncActions(GET_POSTS, "posts", true);
 const getPostReducer = handleAsyncActions(GET_POST, "post");
 
 export default function posts(state = initialState, action) {
@@ -34,6 +37,11 @@ export default function posts(state = initialState, action) {
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
       return getPostReducer(state, action);
+    case CLEAR_POST:
+      return {
+        ...state,
+        post: reducerUtils.initial(),
+      };
     default:
       return state;
   }
