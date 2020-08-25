5c5136d9c03179c4af3ea7606934572fdfeaa164
Junnna <gud0415@gmail.com>
2020-08-25 13:05:44 +0900

    feat : study connect router and read specific post

--- Fast_Campus/React/project/learn-redux-middleware/src/App.js
+++ Fast_Campus/React/project/learn-redux-middleware/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import PostListContainer from "./containers/PostListContainer";
+import { Route } from "react-router-dom";
+import PostListPage from "./pages/PostListPage";
+import PostPage from "./pages/PostPage";
 
 function App() {
-  return <PostListContainer />;
+  return (
+    <>
+      <Route path="/" component={PostListPage} exact={true} />
+      <Route path="/:id" component={PostPage} />
+    </>
+  );
 }
 
 export default App;
