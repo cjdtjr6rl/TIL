aacad864c049cf21350bc19fe568a3870a41b624
Junnna <gud0415@gmail.com>
2020-08-31 16:55:47 +0900

    feat : study saga connect Router

--- Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
+++ Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
@@ -6,7 +6,7 @@ import {
   createPromiseSaga,
   createPromiseSagaById,
 } from "../lib/asyncUtils";
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, getContext } from "redux-saga/effects";
 
 const GET_POSTS = "GET_POSTS";
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
@@ -15,6 +15,7 @@ const GET_POSTS_ERROR = "GET_POSTS_ERROR";
 const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
+const GO_TO_HOME = "GO_TO_HOME";
 
 const CLEAR_POST = "CLEAR_POST";
 
@@ -27,15 +28,18 @@ export const getPost = (id) => ({
 
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
+function* goToHomeSaga() {
+  const history = yield getContext("history");
+  history.push("/");
+}
 
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
 }
 
-export const goToHome = () => (dispatch, getState, { history }) => {
-  history.push("/");
-};
+export const goToHome = () => ({ type: GO_TO_HOME });
 
 export const clearPost = () => ({ type: CLEAR_POST });
 
