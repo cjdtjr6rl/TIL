a1a84d20a450af1b7f41f89d03dd21a0dbdedabe
Junnna <gud0415@gmail.com>
2020-08-31 17:02:29 +0900

    feat : study saga use select

--- Fast_Campus/React/project/learn-redux-middleware/src/containers/PostContainer.js
+++ Fast_Campus/React/project/learn-redux-middleware/src/containers/PostContainer.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Post from "../components/Post";
-import { getPost, goToHome } from "../modules/posts";
+import { getPost, goToHome, printState } from "../modules/posts";
 import { reducerUtils } from "../lib/asyncUtils";
 
 function PostContainer({ postId }) {
@@ -22,6 +22,7 @@ function PostContainer({ postId }) {
   return (
     <>
       <button onClick={() => dispatch(goToHome())}>홈으로 이동</button>
+      <button onClick={() => dispatch(printState())}>상태 출력</button>
       <Post post={data} />
     </>
   );
--- Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
+++ Fast_Campus/React/project/learn-redux-middleware/src/modules/posts.js
@@ -6,7 +6,7 @@ import {
   createPromiseSaga,
   createPromiseSagaById,
 } from "../lib/asyncUtils";
-import { takeEvery, getContext } from "redux-saga/effects";
+import { takeEvery, getContext, select } from "redux-saga/effects";
 
 const GET_POSTS = "GET_POSTS";
 const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
@@ -18,6 +18,7 @@ const GET_POST_ERROR = "GET_POST_ERROR";
 const GO_TO_HOME = "GO_TO_HOME";
 
 const CLEAR_POST = "CLEAR_POST";
+const PRINT_STATE = "PRINT_STATE";
 
 export const getPosts = () => ({ type: GET_POSTS });
 export const getPost = (id) => ({
@@ -25,6 +26,7 @@ export const getPost = (id) => ({
   payload: id,
   meta: id,
 });
+export const printState = () => ({ type: PRINT_STATE });
 
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
@@ -32,11 +34,16 @@ function* goToHomeSaga() {
   const history = yield getContext("history");
   history.push("/");
 }
+function* printStateSaga() {
+  const state = yield select((state) => state.posts);
+  console.log(state);
+}
 
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
   yield takeEvery(GO_TO_HOME, goToHomeSaga);
+  yield takeEvery(PRINT_STATE, printStateSaga);
 }
 
 export const goToHome = () => ({ type: GO_TO_HOME });
