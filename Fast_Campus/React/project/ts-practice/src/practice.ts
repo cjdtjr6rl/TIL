b50f189c5f44f82beed02eb09da1e4403b5bbe10
Junnna <gud0415@gmail.com>
2020-09-15 14:00:26 +0900

    feat : study type (typescript)

---
+++ Fast_Campus/React/project/ts-practice/src/practice.ts
@@ -0,0 +1,14 @@
+let count = 0;
+count += 1;
+
+const message: string = "hello world";
+const done: boolean = true;
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ["hello", "world", "!"];
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: "red" | "orange" | "yellow" = "red";
+color = "yellow";
