fff59c49e342fe54b9b1e30d282e7cb2fdf01020
Junnna <gud0415@gmail.com>
2020-09-18 15:30:10 +0900

    feat : interface make objects

--- Fast_Campus/React/project/ts-practice/src/practice.ts
+++ Fast_Campus/React/project/ts-practice/src/practice.ts
@@ -1,30 +1,18 @@
-interface Shape {
-  getArea(): number;
+interface Person {
+  name: string;
+  age?: number;
 }
 
-class Circle implements Shape {
-  constructor(public radius: number) {}
-
-  getArea() {
-    return this.radius * this.radius * Math.PI;
-  }
+interface Developer extends Person {
+  skills: string[];
 }
 
-class Rectangle implements Shape {
-  constructor(private width: number, private height: number) {
-    this.width = width;
-    this.height = height;
-  }
+const person: Person = {
+  name: "Junnna",
+  age: 27,
+};
 
-  getArea() {
-    return this.width * this.height;
-  }
-}
-
-const circle = new Circle(5);
-const rectangle = new Rectangle(2, 5);
-const shapes: Shape[] = [circle, rectangle];
-
-shapes.forEach((shape) => {
-  console.log(shape.getArea());
-});
+const expert: Developer = {
+  name: "Hyoung",
+  skills: ["JavaScript", "React", "TypeScript"],
+};
