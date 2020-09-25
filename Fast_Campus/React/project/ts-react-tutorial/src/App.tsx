e0612f5a4cb9769b50d1ca4810803b9056534f60
Junnna <gud0415@gmail.com>
2020-09-25 14:45:34 +0900

    feat : study useState (typescript)

--- Fast_Campus/React/project/ts-react-tutorial/src/App.tsx
+++ Fast_Campus/React/project/ts-react-tutorial/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Greetings from "./Greetings";
+import MyForm from "./MyForm";
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log({ name });
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
   };
-  return <Greetings name="Junnna" onClick={onClick} />;
+  return <MyForm onSubmit={onSubmit} />;
 };
 
 export default App;
