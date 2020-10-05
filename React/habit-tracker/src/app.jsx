356e1fbafc8efca72b7950021ab7702f2a192062
Junnna <gud0415@gmail.com>
2020-10-05 12:10:02 +0900

    feat : all habit count resetl

--- React/habit-tracker/src/app.jsx
+++ React/habit-tracker/src/app.jsx
@@ -33,6 +33,13 @@ class App extends Component {
     const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
     this.setState({ habits });
   };
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  };
 
   render() {
     return (
@@ -46,6 +53,7 @@ class App extends Component {
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
           onAdd={this.handleAdd}
+          onReset={this.handleReset}
         />
       </>
     );
--- React/habit-tracker/src/components/habits.jsx
+++ React/habit-tracker/src/components/habits.jsx
@@ -18,6 +18,9 @@ class Habits extends Component {
             />
           ))}
         </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
       </>
     );
   }
