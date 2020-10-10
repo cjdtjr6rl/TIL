f546ee2482c2e55b38e249a31c9516510cc4ec45
Junnna <gud0415@gmail.com>
2020-10-10 17:11:43 +0900

    feat : edit media query

--- React/youtube/src/service/youtube.js
+++ React/youtube/src/service/youtube.js
@@ -32,14 +32,6 @@ class Youtube {
       ...item,
       id: item.id.videoId,
     }));
-    // const response = await fetch(
-    //   // query의 값이 search하는 값
-    //   // props에서 넘어갔을 때 값이 들어갈 것임
-    //   `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=${query}&type=video&key=${this.key}`,
-    //   this.getRequestOptions
-    // );
-    // const result = await response.json();
-    // return result.items.map((item) => ({ ...item, id: item.id.videoId }));
   }
 }
 
