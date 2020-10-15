01f6cc88fe1e045d592b9e45186f011d08c2906f
Junnna <gud0415@gmail.com>
2020-10-15 15:30:06 +0900

    feat : add card add form component

--- React/card-maker/src/components/preview/preview.jsx
+++ React/card-maker/src/components/preview/preview.jsx
@@ -4,7 +4,7 @@ import styles from './preview.module.css';
 
 const Preview = ({cards}) => (
     <section className={styles.preview}>
-        <h1 className={styles.title}>Card Maker</h1>
+        <h1 className={styles.title}>Card Preview</h1>
             <ul className={styles.cards}>
                 {cards.map(card => (
                     <Card key={card.id} card={card} />
