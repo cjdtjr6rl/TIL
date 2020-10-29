41cc31c644b383a81b94b03ec33d6a02eee7e916
Junnna <gud0415@gmail.com>
2020-10-29 13:18:58 +0900

    feat : click to print card

--- React/card-maker/src/components/card/card.jsx
+++ React/card-maker/src/components/card/card.jsx
@@ -8,11 +8,28 @@ const Card = memo(({ card }) => {
     const prinfRef= useRef();
 
     function print() {
-        console.log(prinfRef.current.outerText + ' 프린트 할거얏!');
+        const html = document.querySelector('html');
+        const printContents = prinfRef.current.innerHTML;
+        const printDiv = document.createElement('li');
+        printDiv.className = "print-div";
+        printDiv.style.display = 'flex';
+        printDiv.style.alignItems = 'center';
+        printDiv.style.width = '100%';
+        printDiv.style.marginBottom = '0.5em';
+        printDiv.style.padding =  '0.2em 0';
+        printDiv.style.boxShadow = '6px 6px 8px 0px makerShadow';
+        printDiv.style.maxWidth = '25rem';
+
+        html.appendChild(printDiv);
+        printDiv.innerHTML = printContents;
+        document.body.style.display = 'none';
+        window.print();
+        document.body.style.display = 'block';
+        printDiv.style.display = 'none';
     }
 
     return (
-        <li ref={prinfRef} className={`${styles.card} ${getStyles(theme)} ${shapeStyles(shape)}`} onClick={print}>
+        <li id="modal-body" ref={prinfRef} className={`${styles.card} ${getStyles(theme)} ${shapeStyles(shape)}`} onClick={print}>
             <img className={styles.avatar} src={url} alt="profile"/>
             <div className={styles.info}>
                 <h1 className={styles.name}>{name}</h1>
