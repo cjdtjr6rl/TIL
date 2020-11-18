0572f1af177f4538b49d3ea3ac7252cd5aaa6cca
Junnna <gud0415@gmail.com>
2020-11-18 14:38:24 +0900

    feat : click to target name

--- React/card-maker/src/components/usertalk/usertalk.jsx
+++ React/card-maker/src/components/usertalk/usertalk.jsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 import styles from './usertalk.module.css';
 
 const UserTalk = memo(({ myId, user }) => {
+    const onClick = () => {
+        console.log(user);
+    }
     return (
         <>
             { user !== myId ?
                 (<Link className={styles.link} to={`/${user}`}>
-                        <li className={styles.user}>
+                        <li className={styles.user} onClick={onClick}>
                         {user}
                     </li>
                 </Link>) : <div className={styles.none}></div>
