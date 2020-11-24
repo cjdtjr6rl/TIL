2a7e048d7df1350d5a4de5dce2983a060d5bc956
Junnna <gud0415@gmail.com>
2020-11-24 16:08:14 +0900

    feat : create redux file

---
+++ Fast_Campus/React/project/repeat-learn-redux-middleware/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CounterContainer from "./containers/CounterContainer";
+
+function App() {
+  return <CounterContainer />;
+}
+
+export default App;
